ba39942c740353eb19916f3d4e3f65dc3ea620af
Denis Selivanov <innowise@innowises-Mac-mini.local>
2023-08-24 13:22:25 +0200

    initial commit

---
+++ GoogMapsTracker/GoogMapsTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogMapsTracker
+//
+//  Created by innowise on 8/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
